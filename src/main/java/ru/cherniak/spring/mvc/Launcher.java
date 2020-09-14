9ba25bded83c18585a6aefee8fcfa3fa8a6fd9d4
Viktor Cherniak <barsetca@gmail.com>
2020-09-14 10:53:56 +0300

    Initial commit

---
+++ src/main/java/ru/cherniak/spring/mvc/Launcher.java
@@ -0,0 +1,26 @@
+package ru.cherniak.spring.mvc;
+
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.webapp.WebAppContext;
+
+import java.net.URL;
+import java.security.ProtectionDomain;
+
+public class Launcher {
+    public static void main(String[] args) throws Exception {
+        Server server = new Server(8189);
+
+        ProtectionDomain domain = Launcher.class.getProtectionDomain();
+        URL location = domain.getCodeSource().getLocation();
+
+        WebAppContext webAppContext = new WebAppContext();
+// http://localhost:8189/my-mvc/*
+        webAppContext.setContextPath("/my-mvc");
+
+        webAppContext.setWar(location.toExternalForm());
+
+        server.setHandler(webAppContext);
+        server.start();
+        server.join();
+    }
+}
